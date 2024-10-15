e133f6f88081191b8fda829e8c1dcd1cf428504d
Shkkilet <mguk704@gmail.com>
2024-10-15 13:54:07 +0300

    modified js

--- lab3.js
+++ lab3.js
@@ -4,7 +4,6 @@ const { Command } = require('commander');
 
 const program = new Command();
 
-// Налаштування командного рядка
 program
     .requiredOption('-i, --input <path>', 'шлях до файлу для читання (обовʼязковий параметр)')
     .option('-o, --output <path>', 'шлях до файлу для запису результату')
@@ -14,24 +13,21 @@ program.parse(process.argv);
 
 const options = program.opts();
 
-// Функція для виведення повідомлень про помилки та завершення програми
+
 function exitWithError(message) {
     console.error(message);
     process.exit(1);
 }
 
-// Перевірка, чи існує файл для читання
 const inputPath = path.resolve(options.input);
 if (!fs.existsSync(inputPath)) {
     exitWithError("Cannot find input file");
 }
 
 try {
-    // Читання JSON-файлу
     const rawData = fs.readFileSync(inputPath, 'utf-8');
     const jsonData = JSON.parse(rawData);
 
-    // Пошук активу з найменшим значенням
     let minAsset = jsonData[0];
     jsonData.forEach(asset => {
         if (Number(asset.value) < Number(minAsset.value)) {
@@ -39,10 +35,8 @@ try {
         }
     });
 
-    // Формування рядка для виводу
     const outputString = `${minAsset.txt}:${minAsset.value}`;
 
-    // Обробка виводу залежно від параметрів командного рядка
     if (options.output) {
         const outputPath = path.resolve(options.output);
         fs.writeFileSync(outputPath, outputString, 'utf-8');
@@ -52,7 +46,6 @@ try {
         console.log(outputString);
     }
 
-    // Якщо жоден з необов'язкових параметрів не задано - нічого не виводимо
 } catch (error) {
     exitWithError(`Error processing the file: ${error.message}`);
 }
